f6c977a295c2713fdf22be11cd578557ae85f935
nseraphin <seraphinnina@gmail.com>
2019-10-31 13:32:47 -0400

    CSS version mobile de page Compte

--- api/vues/accueil/monCompte.html.php
+++ api/vues/accueil/monCompte.html.php
@@ -11,16 +11,22 @@
               else if (isset($_GET['update']) && $_GET['update']=="ok"){
                 echo "Votre mot de passe a été modifié.";  
               } ?></p>
-           <h4>INFOS PERSONNELLES</h4>
-            <p>Nom d'utilisateur :
-                <?php echo $_SESSION["username"]; ?>
-            </p>
-            <p>Mot de passe :
-                <?php 
-            echo '*********'; 
-            ?>
-            </p>
-            <a href="#" id='test'>Modifier mon mot de passe</a>
+           <h2>INFORMATIONS PERSONNELLES</h2>
+           <section class="infoPerso">
+               <div class="elmt">
+                    <p class="txtBtn">Nom d'utilisateur :</p>
+                    <p> <?php echo $_SESSION["username"]; ?></p>
+               </div>
+              <div class="elmt">
+                <p class="txtBtn">Mot de passe :</p>
+                <p> <?php 
+                echo '*********'; 
+                ?></p>
+                <a href="#" id='test' class="txtLien">Modifier mon mot de passe ></a>
+              </div>
+
+           </section>
+            
         </div>
 
         <form action="/art-public-mtl/api/compte/modifierPW" id="form" method="post">
@@ -29,31 +35,34 @@
                     <input type="hidden" value='<?php echo $password; ?>' name='oldPW' id='oldPW'>
                 </div>
                 <div>
-                    <label for="">Nouveau mot de passe :</label>
-                    <input type="password" value='' name='newPW' id="newPW">   
+                    <label class="txtBtn"for="">Nouveau mot de passe :</label>
+                    <input class="inputText" type="password" value='' name='newPW' id="newPW">   
                     <p id="msgErreurRegex"></p>
                 </div>
                 <div>
-                    <label for="">Confirmer le nouveau mot de passe :</label>
-                    <input type="password" value='' name='confirmNewPW' id='confirmNewPW'>
+                    <label class="txtBtn" for="">Confirmer le nouveau mot de passe :</label>
+                    <input  class="inputText" type="password" value='' name='confirmNewPW' id='confirmNewPW'>
                     <p id="msgErreurConfirm"></p>
                 </div>
-                <div id=btns_form>
-                    <input type="button" id="annuler" value="Annuler">
-                    <input type="submit" id="modifier" value="Modifier">
+                <div id=btns_form class="btns_form">
+                    <input class="btn annuler" type="button" id="annuler" value="Annuler">
+                    <input class="btn" type="submit" id="modifier" value="Modifier">
                 </div>
             </div>
         </form>
     </div>
     <div class="mesDonnes">
       <div class="autresInfos">
-        <h4>AUTRES INFOS</h4>
-        <p>Adresse mail : <?php echo $courriel; ?></p>
+        <h2>AUTRES INFOS</h2>
+        <section class="infoPerso">
+            <p class="txtBtn">Adresse mail : </p>
+            <p><?php echo $courriel; ?></p>
+        </section>
       </div>
     </div>
-            <form id='deconnexion' action="/art-public-mtl/api/compte/deconnexion" method="post">	
+            <form class="deco" id='deconnexion' action="/art-public-mtl/api/compte/deconnexion" method="post">	
                 <div>
-                    <input type="submit" id="envoyer" value="Déconnexion">
+                    <input type="submit" id="envoyer" value="Déconnexion" class="btn">
                 </div>		
             </form>
 
